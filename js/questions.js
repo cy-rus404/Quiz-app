4d6655f7856c8b0e9c6e618c9c7d7c501dbd9c23
isaac.Adumoah <isaac.sduisaac@gmail.com>
2022-07-23 17:52:59 +0000

    initial commit of site

---
+++ js/questions.js
@@ -0,0 +1,94 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the third day of the week?",
+    answer: "Wednesday",
+    options: [
+      "Monday",
+      "Friday",
+      "Wednesday",
+      "Sunday"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which month has 28 or 29 days?",
+    answer: "February",
+    options: [
+      "January",
+      "December",
+      "October",
+      "February"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many months are in a year?",
+    answer: "12",
+    options: [
+      "20",
+      "12",
+      "15",
+      "7"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the sixth month?",
+    answer: "June",
+    options: [
+      "May",
+      "August",
+      "June",
+      "January"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many days are in a year?",
+    answer: "365 days",
+    options: [
+      "300 days",
+      "360 days",
+      "365 days",
+      "366 days"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "How many days make a leap year",
+    answer: "365/4 days",
+    options: [
+      "366/4 days",
+      "364/2 days",
+      "365/4 days",
+      "299/3 days"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How many days are in a week",
+    answer: "7",
+    options: [
+      "6",
+      "3",
+      "7",
+      "9"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many weeks are in a month",
+    answer: "4",
+    options: [
+      "6",
+      "3",
+      "4",
+      "9"
+    ]
+  },
+];
